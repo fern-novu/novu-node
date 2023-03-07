110ccb8f9dbabc865b4c0d9a071f8358b91b916c
dsinghvi <deep@usebirch.com>
2023-03-06 16:54:45 -0800

    start copying types

--- src/Client.ts
+++ src/Client.ts
@@ -23,6 +23,7 @@ import { Activity } from "./api/resources/activity/client/Client";
 import { Notification } from "./api/resources/notification/client/Client";
 import { Feeds } from "./api/resources/feeds/client/Client";
 import { Messages } from "./api/resources/messages/client/Client";
+import { ChannelPreferenceType } from "api/resources/commons/types";
 
 export declare namespace NovuClient {
     interface Options {
@@ -41,7 +42,7 @@ export class NovuClient {
      *     Additional information can be passed according the body interface below.
      *
      */
-    public async trigger(request: Novu.TriggerEventRequestDto): Promise<void> {
+    public async trigger(eventId: string, data: ITriggerPayloadOptions): Promise<void> {
         const _response = await core.fetcher({
             url: urlJoin(this.options.environment, "/v1/events/trigger"),
             method: "POST",
@@ -256,4 +257,150 @@ export class NovuClient {
 
         return undefined;
     }
+}
+
+export interface ISubscriberPayload {
+    firstName?: string;
+    lastName?: string;
+    email?: string;
+    phone?: string;
+    avatar?: string;
+    locale?: string;
+    data?: SubscriberCustomData;
+    [key: string]: string | string[] | boolean | number | SubscriberCustomData | undefined;
+}
+
+export type SubscriberCustomData = {
+    [key: string]: string | string[] | boolean | number | undefined;
+};
+
+export interface ISubscribersDefine extends ISubscriberPayload {
+    subscriberId: string;
+}
+
+export type TriggerRecipientSubscriber = string | ISubscribersDefine;
+
+export type TriggerRecipient = TriggerRecipientSubscriber | ITopic;
+
+export interface ITopic {
+    type: TriggerRecipientsTypeEnum.TOPIC;
+    topicKey: TopicKey;
+  }
+
+export type TriggerRecipients = TriggerRecipient[];
+
+export type TriggerRecipientsPayload = TriggerRecipientSubscriber | TriggerRecipients;
+
+export interface IBroadcastPayloadOptions {
+    payload: ITriggerPayload;
+    overrides?: ITriggerOverrides;
+}
+
+export interface ITriggerPayloadOptions extends IBroadcastPayloadOptions {
+    to: TriggerRecipientsPayload;
+    actor?: TriggerRecipientSubscriber;
+}
+
+export interface ITriggerPayload {
+    attachments?: IAttachmentOptions[];
+    [key: string]:
+        | string
+        | string[]
+        | boolean
+        | number
+        | undefined
+        | IAttachmentOptions
+        | IAttachmentOptions[]
+        | Record<string, unknown>;
+}
+
+export type ITriggerOverrides = {
+    [key in "mailgun" | "nodemailer" | "plivo" | "postmark" | "sendgrid" | "twilio"]?: object;
+} & {
+    [key in "fcm"]?: ITriggerOverrideFCM;
+} & {
+    [key in "apns"]?: ITriggerOverrideAPNS;
+} & {
+    [key in "delay"]?: ITriggerOverrideDelayAction;
+};
+
+export type ITriggerOverrideDelayAction = {
+    unit: DigestUnitEnum;
+    amount: number;
+};
+
+export type ITriggerOverrideFCM = {
+    type?: "notification" | "data";
+    tag?: string;
+    body?: string;
+    icon?: string;
+    badge?: string;
+    color?: string;
+    sound?: string;
+    title?: string;
+    bodyLocKey?: string;
+    bodyLocArgs?: string;
+    clickAction?: string;
+    titleLocKey?: string;
+    titleLocArgs?: string;
+    data?: Record<string, any>;
+};
+
+export type IAPNSAlert = {
+    title?: string;
+    subtitle?: string;
+    body: string;
+    "title-loc-key"?: string;
+    "title-loc-args"?: string[];
+    "action-loc-key"?: string;
+    "loc-key"?: string;
+    "loc-args"?: string[];
+    "launch-image"?: string;
+};
+
+export type ITriggerOverrideAPNS = {
+    topic?: string;
+    id?: string;
+    expiry?: number;
+    priority?: number;
+    collapseId?: string;
+    pushType?: string;
+    threadId?: string;
+    payload?: unknown;
+    aps?: {
+        alert?: string | IAPNSAlert;
+        "launch-image"?: string;
+        badge?: number;
+        sound?: string;
+        "content-available"?: undefined | 1;
+        "mutable-content"?: undefined | 1;
+        "url-args"?: string[];
+        category?: string;
+    };
+    rawPayload?: unknown;
+    badge?: number;
+    sound?: string;
+    alert?: string | IAPNSAlert;
+    contentAvailable?: boolean;
+    mutableContent?: boolean;
+    mdm?: string | Record<string, unknown>;
+    urlArgs?: string[];
+};
+
+export interface IBulkEvents extends ITriggerPayloadOptions {
+    name: string;
+}
+
+export interface IAttachmentOptions {
+    mime: string;
+    file: Buffer;
+    name?: string;
+    channels?: ChannelPreferenceType[];
+}
+
+export enum DigestUnitEnum {
+    Seconds = "seconds",
+    Minutes = "minutes",
+    Hours = "hours",
+    Days = "days",
 }
